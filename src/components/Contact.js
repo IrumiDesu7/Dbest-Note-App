d9157e8b844df89c3115e06bbbd522f5c8e59a66
IrumiDesu7 <ikalam89@gmail.com>
2022-07-03 18:55:39 +0800

    add styling to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,14 +5,23 @@ export default function Contact() {
     <div className='contact'>
       <h2>Contact</h2>
       <form>
-        <label htmlFor='name'>Name*</label>
-        <input type='text' name='name' id='name' />
-        <label htmlFor='email'>Email*</label>
-        <input type='email' name='email' id='email' />
-        <label htmlFor='title'>Title</label>
-        <input type='text' name='title' id='title' />
-        <label htmlFor='comment'>Your Message</label>
-        <textarea name='comment' id='comment' cols='30' rows='10' />
+        <div className='top'>
+          <div className='name-input'>
+            <label htmlFor='name'>Name*</label>
+            <input type='text' name='name' id='name' />
+          </div>
+          <div className='email-input'>
+            <label htmlFor='email'>Email*</label>
+            <input type='email' name='email' id='email' />
+          </div>
+        </div>
+        <div className='bottom'>
+          <label htmlFor='title'>Title</label>
+          <input type='text' name='title' id='title' />
+          <label htmlFor='comment'>Your Message</label>
+          <textarea name='comment' id='comment' cols='30' rows='10' />
+        </div>
+        <button type='button'>Post Comment</button>
       </form>
     </div>
   );
