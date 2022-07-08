4a7a7087f78d68e0cb3d7cdede5a284e2580fcd5
IrumiDesu7 <ikalam89@gmail.com>
2022-07-08 19:52:54 +0800

    z-index changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,18 +8,24 @@ export default function Contact() {
         <div className='top'>
           <div className='name-input'>
             <label htmlFor='name'>Name*</label>
-            <input type='text' name='name' id='name' />
+            <input type='text' name='name' id='name' required={true} />
           </div>
           <div className='email-input'>
             <label htmlFor='email'>Email*</label>
-            <input type='email' name='email' id='email' />
+            <input type='email' name='email' id='email' required={true} />
           </div>
         </div>
         <div className='bottom'>
           <label htmlFor='title'>Title</label>
-          <input type='text' name='title' id='title' />
+          <input type='text' name='title' id='title' required={true} />
           <label htmlFor='comment'>Your Message</label>
-          <textarea name='comment' id='comment' cols='30' rows='10' />
+          <textarea
+            name='comment'
+            id='comment'
+            cols='30'
+            rows='10'
+            required={true}
+          />
         </div>
         <button type='button'>Post Comment</button>
       </form>
