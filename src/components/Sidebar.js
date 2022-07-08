ff39ecc4b5b5535fc66c7906e70bb521e25f73e8
IrumiDesu7 <ikalam89@gmail.com>
2022-07-08 19:30:36 +0800

    add hide navbar button. finally

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,16 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import leftArrow from './left.svg';
 
 export default function Sidebar(props) {
+  const [isOpen, setIsOpen] = useState('');
+  const [rotate, setRotate] = useState('');
+
+  const toggleNavbar = () => {
+    setIsOpen(isOpen === '' ? 'hidden' : '');
+    setRotate(isOpen === '' ? 'rotate' : '');
+  };
+
   const noteElements = props.notes.map((note) => (
     <Link to={`${note.id}`} key={note.id}>
       <div
@@ -22,16 +31,23 @@ export default function Sidebar(props) {
       </div>
     </Link>
   ));
+  useEffect(() => {
+    console.log('useEffect in action');
+  });
   return (
-    <section className='pane sidebar'>
+    <section className={`pane sidebar ${isOpen}`}>
       <div className='sidebar--header'>
         <button className='new-note' onClick={props.newNote}>
           Create Note
         </button>
         <hr />
         <h3>Notes</h3>
-        <button className='hide-navbar'>
-          <img src={leftArrow} alt='arrow to hide/show navbar' />
+        <button onClick={toggleNavbar} className='hide-navbar'>
+          <img
+            className={`${rotate}`}
+            src={leftArrow}
+            alt='arrow to hide/show navbar'
+          />
         </button>
         <hr />
       </div>
